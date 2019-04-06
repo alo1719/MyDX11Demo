f9090b8b1f40c1d28ce1e23b9651b30ca918dee4
alo1719 <1335250440@qq.com>
2019-04-06 16:34:37 +0800

    update basic texture related codes

--- Tutorial01.cpp
+++ Tutorial01.cpp
@@ -15,6 +15,7 @@
 #include <d3dcompiler.h>
 #include <directxmath.h>
 #include <directxcolors.h>
+#include "DDSTextureLoader.h"
 #include "resource.h"
 
 using namespace DirectX;
@@ -25,39 +26,55 @@ using namespace DirectX;
 struct SimpleVertex
 {
 	XMFLOAT3 Pos;
-	//XMFLOAT4 Color;
+	//XMFLOAT2 Tex;
 };
 
-//struct ConstantBuffer
-//{
-//	XMMATRIX mWorld;
-//	XMMATRIX mView;
-//	XMMATRIX mProjection;
-//};
+struct CBNeverChanges
+{
+	XMMATRIX mView;
+};
+
+struct CBChangeOnResize
+{
+	XMMATRIX mProjection;
+};
+
+struct CBChangesEveryFrame
+{
+	XMMATRIX mWorld;
+	XMFLOAT4 vMeshColor;
+};
 
 //--------------------------------------------------------------------------------------
 // Global Variables
 //--------------------------------------------------------------------------------------
-HINSTANCE               g_hInst = nullptr;
-HWND                    g_hWnd = nullptr;
-D3D_DRIVER_TYPE         g_driverType = D3D_DRIVER_TYPE_NULL;
-D3D_FEATURE_LEVEL       g_featureLevel = D3D_FEATURE_LEVEL_11_0;
-ID3D11Device*           g_pd3dDevice = nullptr;
-ID3D11Device1*          g_pd3dDevice1 = nullptr;
-ID3D11DeviceContext*    g_pImmediateContext = nullptr;
-ID3D11DeviceContext1*   g_pImmediateContext1 = nullptr;
-IDXGISwapChain*         g_pSwapChain = nullptr;
-IDXGISwapChain1*        g_pSwapChain1 = nullptr;
-ID3D11RenderTargetView* g_pRenderTargetView = nullptr;
-ID3D11VertexShader*     g_pVertexShader = nullptr;
-ID3D11PixelShader*      g_pPixelShader = nullptr;
-ID3D11InputLayout*      g_pVertexLayout = nullptr;
-ID3D11Buffer*           g_pVertexBuffer = nullptr;
-//ID3D11Buffer*           g_pIndexBuffer = nullptr;
-//ID3D11Buffer*           g_pConstantBuffer = nullptr;
-//XMMATRIX                g_World;
-//XMMATRIX                g_View;
-//XMMATRIX                g_Projection;
+HINSTANCE                   g_hInst = nullptr;
+HWND                        g_hWnd = nullptr;
+D3D_DRIVER_TYPE             g_driverType = D3D_DRIVER_TYPE_NULL;
+D3D_FEATURE_LEVEL           g_featureLevel = D3D_FEATURE_LEVEL_11_0;
+ID3D11Device*               g_pd3dDevice = nullptr;
+ID3D11Device1*              g_pd3dDevice1 = nullptr;
+ID3D11DeviceContext*        g_pImmediateContext = nullptr;
+ID3D11DeviceContext1*       g_pImmediateContext1 = nullptr;
+IDXGISwapChain*             g_pSwapChain = nullptr;
+IDXGISwapChain1*            g_pSwapChain1 = nullptr;
+ID3D11RenderTargetView*     g_pRenderTargetView = nullptr;
+ID3D11Texture2D*            g_pDepthStencil = nullptr;
+ID3D11DepthStencilView*     g_pDepthStencilView = nullptr;
+ID3D11VertexShader*         g_pVertexShader = nullptr;
+ID3D11PixelShader*          g_pPixelShader = nullptr;
+ID3D11InputLayout*          g_pVertexLayout = nullptr;
+ID3D11Buffer*               g_pVertexBuffer = nullptr;
+ID3D11Buffer*               g_pIndexBuffer = nullptr;
+ID3D11Buffer*               g_pCBNeverChanges = nullptr;
+ID3D11Buffer*               g_pCBChangeOnResize = nullptr;
+ID3D11Buffer*               g_pCBChangesEveryFrame = nullptr;
+ID3D11ShaderResourceView*   g_pTextureRV = nullptr;
+ID3D11SamplerState*         g_pSamplerLinear = nullptr;
+XMMATRIX                    g_World;
+XMMATRIX                    g_View;
+XMMATRIX                    g_Projection;
+XMFLOAT4                    g_vMeshColor(0.7f, 0.7f, 0.7f, 1.0f);
 
 //--------------------------------------------------------------------------------------
 // Forward declarations
@@ -153,6 +170,7 @@ HRESULT InitWindow( HINSTANCE hInstance, int nCmdShow )
 // Helper for compiling shaders with D3DCompile
 //
 // With VS 11, we could load up prebuilt .cso files instead...
+// 一般而言此函数也不需要改动
 //--------------------------------------------------------------------------------------
 HRESULT CompileShaderFromFile(const WCHAR* szFileName, LPCSTR szEntryPoint, LPCSTR szShaderModel, ID3DBlob** ppBlobOut)
 {
